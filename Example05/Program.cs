e5a611c602c6a58a6331f9f0029321deccff03ac
NorrinRadd <EpicEver94@gmail.com>
2022-07-09 01:23:05 +0300

    Exmple05 приветсвенное встреча любимчика

---
+++ Example05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "арген")
+{
+  Console.WriteLine("Да здравствует наш Король");  
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
